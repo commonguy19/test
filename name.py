92314bce948cfd32ea03a90a7fd8601715cfcb60
unknown <commonguy19@gmail.com>
2020-05-07 18:18:57 +0530

    nice addition

--- name.py
+++ name.py
@@ -1,9 +1,7 @@
 name = input("Enter your name: ")
 print ("Hi " + name)
 print("Welcome to the git World")
-<<<<<<< HEAD
 print("Nice to have you here")
-=======
 gender = input("Are you a Male/Female: ")
 if gender == "Male":
     print ("Hey Dude")
